d31886d58d365ba6679b175a275426837e1ed4e0
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2023-08-16 18:00:29 +0200

    Remove unused RemoveFromGroups hotkey

--- McBoomsMod/hook/lua/keymap/keymapper.lua
+++ McBoomsMod/hook/lua/keymap/keymapper.lua
@@ -1,4 +1,4 @@
 local modFolder = 'McBoomsMod'
 local mod_category = 'McBoomBoomsMod'
 local order = 1
-SetUserKeyAction("Remove from control group", {action = "UI_Lua import('/mods/"..modFolder.."/modules/util/Util.lua').RemoveFromGroups()", category = mod_category, order = order})
+-- SetUserKeyAction("Remove from control group", {action = "UI_Lua import('/mods/"..modFolder.."/modules/util/Util.lua').RemoveFromGroups()", category = mod_category, order = order})
