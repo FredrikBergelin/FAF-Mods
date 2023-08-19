97fe554d81d8f7f8772221338c7013b9362b1b19
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2023-08-19 19:04:16 +0200

    Remove unused mods

--- Select Air Scouts/hook/lua/ui/game/gamemain.lua
+++
@@ -1,2 +0,0 @@
-local KeyMapper = import('/lua/keymap/keymapper.lua')
-KeyMapper.SetUserKeyAction('Select Air Scouts', {action = "UI_SelectByCategory AIR INTELLIGENCE", category = 'selection', order = 35})
