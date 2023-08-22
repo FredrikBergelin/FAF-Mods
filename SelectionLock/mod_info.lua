e0df51155621571e2a53bd2826058b5670f42137
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2023-08-23 00:54:39 +0200

    Info update + Formatting

--- SelectionLock/mod_info.lua
+++ SelectionLock/mod_info.lua
@@ -1,10 +1,10 @@
 name = "Selection Lock"
-version = 4.0
-copyright = "nah"
-description = "Prevents selection of assisting units. You can still select one at a time. Can configure to limit by unit type. Special thanks to yorick + Sir Prize + Crotalus + Coding Squirrel and Morax"
-author = "partytime"
+version = 1
+copyright = ""
+description = "Prevents selection of assisting units. You can still select one at a time. Can configure to limit by unit type. Based on Selection Lock v4"
+author = "FreadyFish"
 url = ""
-uid = "9C801555-8FAA-47EA-3333-8AC9EE16B070"
+uid = "selection-lock-ff-v1"
 exclusive = false
 ui_only =true
 conflicts = {}
