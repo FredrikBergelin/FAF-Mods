7519d7ab5d018810264c3900d9280f5947e43e86
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2023-08-28 21:08:18 +0200

    Move tLOG to common mod tools

--- patrol2move/modules/module.lua
+++ patrol2move/modules/module.lua
@@ -2,33 +2,6 @@ local trackedUnits = {}
 local catCache = {}
 local numrepeat = 20
 
-function tLOG(tbl, indent)
-	if not indent then indent = 0 end
-	formatting = string.rep("  ", indent)
-	if type(tbl) == "nil" then
-		LOG(formatting .. "nil")
-		return
-	end
-	if type(tbl) == "string" then
-		LOG(formatting .. tbl)
-		return
-	end
-	for k, v in pairs(tbl) do
-		formatting = string.rep("  ", indent) .. k .. ": "
-		if type(v) == "nil" then
-			LOG(formatting .. "NIL")
-		elseif type(v) == "table" then
-			LOG(formatting)
-			tLOG(v, indent + 1)
-		elseif type(v) == 'boolean' then
-			LOG(formatting .. tostring(v))
-		else
-			LOG(formatting)
-			LOG(v)
-		end
-	end
-end
-
 function ConvertToMove()
 	local units = GetSelectedUnits() or {}
 
