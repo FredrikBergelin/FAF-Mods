17b1118dbf162fea7d32f7a0e64bc2ca20ffbf66
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2024-05-27 20:05:53 +0200

    More Hotkey Layouts v3

---
+++ More Hotkey Layouts/mod_info.lua
@@ -0,0 +1,8 @@
+name = "More Hotkey Layouts"
+uid = "cf8b500a-0ca5-11ec-lp8a-346faca3452eff28"
+version = 3
+description = "Adds additional hotkey layouts, so you can toggle between different configurable sets of keybindings"
+author = "Emperor_Penguin"
+exclusive = false
+ui_only = true
+icon = "/mods/More Hotkey Layouts/icon.png"
