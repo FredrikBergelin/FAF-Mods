f55701444b14a0ad3717e935bf80275eace63b52
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2024-07-24 15:13:04 +0200

    HotBuildCycleFix

---
+++ HotBuildCycleFix/mod_info.lua
@@ -0,0 +1,18 @@
+name = "HotBuildCycleFix"
+uid = "020f0b6d-0a86-4afa-b2e1-0a0afdc3767d"
+version = 4
+copyright = ""
+description = "Fixes the issue of hotbuild cycle not resetting if you press the same key again."
+author = "Drimocus"
+url = ""
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+requires = {}
+requiresNames = {}
+conflicts = {}
+-- if these mods are enabled our mod is loaded before them
+before = {}
+-- if these mods are enabled our mod is loaded after them
+after = {}
